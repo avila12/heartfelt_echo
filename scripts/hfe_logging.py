4ae2219fc5c6ab8aea1453a3d93167cfa72ff9e7
alexvila <avila@play2pay.com>
2025-01-04 21:35:10 -0500

    logging

--- scripts/hfe_logging.py
+++ scripts/hfe_logging.py
@@ -1,17 +1,20 @@
 import logging
 from logging.handlers import RotatingFileHandler
+from cloghandler import ConcurrentRotatingFileHandler  # pip install ConcurrentLogHandler
 
 
 def configure_logging():
-    # Create a logger
+    # Create logger
     logger = logging.getLogger("flask_app")
-    logger.setLevel(logging.DEBUG)  # Adjust log level as needed
+    logger.setLevel(logging.ERROR)
 
     # Define log formatter
-    formatter = logging.Formatter("%(asctime)s %(levelname)s [%(name)s]: %(message)s")
+    formatter = logging.Formatter(
+        "%(asctime)s %(levelname)s [%(process)d]: %(message)s"
+    )
 
-    # Create a rotating file handler (5MB max size, 5 backup files)
-    file_handler = RotatingFileHandler(
+    # Concurrent rotating file handler
+    file_handler = ConcurrentRotatingFileHandler(
         "flask.log", maxBytes=5 * 1024 * 1024, backupCount=5
     )
     file_handler.setLevel(logging.DEBUG)
@@ -20,17 +23,10 @@ def configure_logging():
     # Add the handler to the logger
     logger.addHandler(file_handler)
 
-    # Optionally log to console
+    # Optionally, log to console
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.INFO)  # Use INFO for less verbose logs
+    console_handler.setLevel(logging.INFO)
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
-
-    # Prevent Flask's default logger from duplicating logs
-    flask_logger = logging.getLogger("werkzeug")
-    flask_logger.setLevel(
-        logging.ERROR
-    )  # Silence Werkzeug logs or set appropriate level
-    flask_logger.handlers = [file_handler, console_handler]
 
     return logger
