c8646dd45ac3b8173589a70d0498ce53b55cad19
alexvila <avila@play2pay.com>
2025-01-04 19:20:42 -0500

    testing apscheduler

--- app.py
+++ app.py
@@ -16,6 +16,32 @@ from scripts.weatherapi import (
 load_dotenv()
 os.environ["DISPLAY"] = ":0"
 
+import atexit
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("apscheduler").setLevel(logging.DEBUG)
+
+# Scheduler setup to turn on/off monitor
+scheduler = BackgroundScheduler()
+scheduler.add_job(
+    lambda: set_monitor_state("on_rotate_left"),
+    'cron',
+    hour=8,
+    minute=0,
+    misfire_grace_time=60  # Allow a 60-second grace period
+)  # Turn on at 7:00 AM
+scheduler.add_job(
+    lambda: set_monitor_state("off"),
+    'cron',
+    hour=19,
+    minute=23,
+    misfire_grace_time=60  # Allow a 60-second grace period
+)  # Turn off at 10:00 PM
+
+scheduler.start()
+atexit.register(lambda: scheduler.shutdown())
+
 app = Flask(__name__)
 
 zipcode = os.getenv('zipcode', '33615')
@@ -133,13 +159,7 @@ def serve_photo(filename):
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-import atexit
-# Scheduler setup to turn on/off monitor
-scheduler = BackgroundScheduler()
-scheduler.add_job(set_monitor_state, 'cron', hour=19, minute=12, args=["off"])  # Turn off at 10:00 PM
-scheduler.add_job(set_monitor_state, 'cron', hour=8, minute=0, args=["on_rotate_left"])   # Turn on at 7:00 AM
-scheduler.start()
-atexit.register(lambda: scheduler.shutdown())
+
 
 @app.route("/set-monitor-state-off")
 def set_monitor_state_off():
