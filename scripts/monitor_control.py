a66f185443cef72ca53d9f8198a071e67c8535c8
alexvila <avila@play2pay.com>
2025-01-04 21:04:29 -0500

    removed logging

--- scripts/monitor_control.py
+++ scripts/monitor_control.py
@@ -1,11 +1,4 @@
 import subprocess
-import logging
-
-logging.basicConfig(
-    filename="flask.log",  # Log file
-    level=logging.DEBUG,  # Log level
-    format="%(asctime)s %(levelname)s: %(message)s",
-)
 
 
 def set_monitor_state(state):
@@ -13,7 +6,6 @@ def set_monitor_state(state):
     Controls the monitor state using xrandr.
     :param state: "off", "on_rotate_left"
     """
-    logging.debug(f"Setting monitor state to: {state}")
     try:
         if state == "off":
             subprocess.run(["xrandr", "--output", "HDMI-1", "--off"], check=True)
