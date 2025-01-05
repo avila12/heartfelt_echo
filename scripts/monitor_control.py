a2f8cae197e653ed8dd7391323d637952a8f9022
alexvila <avila@play2pay.com>
2025-01-04 21:06:26 -0500

    added logging

--- scripts/monitor_control.py
+++ scripts/monitor_control.py
@@ -1,4 +1,11 @@
 import subprocess
+import logging
+
+logging.basicConfig(
+    filename="flask.log",  # Log file
+    level=logging.DEBUG,  # Log level
+    format="%(asctime)s %(levelname)s: %(message)s",
+)
 
 
 def set_monitor_state(state):
@@ -6,6 +13,7 @@ def set_monitor_state(state):
     Controls the monitor state using xrandr.
     :param state: "off", "on_rotate_left"
     """
+    logging.debug(f"Setting monitor state to: {state}")
     try:
         if state == "off":
             subprocess.run(["xrandr", "--output", "HDMI-1", "--off"], check=True)
@@ -15,6 +23,6 @@ def set_monitor_state(state):
                 check=True,
             )
         else:
-            print(f"Unknown state: {state}")
+            logging.debug(f"Unknown state: {state}")
     except subprocess.CalledProcessError as e:
-        print(f"Failed to set monitor state to '{state}': {e}")
+        logging.debug(f"Failed to set monitor state to '{state}': {e}")
