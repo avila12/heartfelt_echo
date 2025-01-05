04b3c89c71027a550e7f90619b02b581d8c09455
alexvila <avila@play2pay.com>
2025-01-04 20:58:14 -0500

    new utils file

---
+++ scripts/utils.py
@@ -0,0 +1,8 @@
+
+from datetime import datetime
+
+# Function to convert 12-hour clock to 24-hour clock
+def convert_to_24_hour(time_str):
+    if time_str:
+        return datetime.strptime(time_str, "%I:%M %p").strftime("%H:%M")
+    return None
