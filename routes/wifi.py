2185c399fb3f459fc05d467d84aa5cc90e2395f0
alexvila <avila@play2pay.com>
2025-01-24 08:21:26 -0500

    wifi

--- routes/wifi.py
+++ routes/wifi.py
@@ -71,24 +71,24 @@ def wifi_index():
     return render_template('wifi.html')
 
 
-# @wifi_bp.route('/scan', methods=['GET'])
-# def scan_networks():
-#     networks = get_available_wifi()
-#     if 'error' in networks:
-#         return jsonify(networks), 500
-#     return jsonify(networks)
-#
-#
-# @wifi_bp.route('/connect', methods=['POST'])
-# def connect_network():
-#     ssid = request.json.get('ssid')
-#     password = request.json.get('password')
-#
-#     if not ssid or not password:
-#         return jsonify({"error": "SSID and password are required."}), 400
-#
-#     response = connect_to_wifi(ssid, password)
-#
-#     if 'error' in response:
-#         return jsonify(response), 500
-#     return jsonify(response)
+@wifi_bp.route('/scan', methods=['GET'])
+def scan_networks():
+    networks = get_available_wifi()
+    if 'error' in networks:
+        return jsonify(networks), 500
+    return jsonify(networks)
+
+
+@wifi_bp.route('/connect', methods=['POST'])
+def connect_network():
+    ssid = request.json.get('ssid')
+    password = request.json.get('password')
+
+    if not ssid or not password:
+        return jsonify({"error": "SSID and password are required."}), 400
+
+    response = connect_to_wifi(ssid, password)
+
+    if 'error' in response:
+        return jsonify(response), 500
+    return jsonify(response)
